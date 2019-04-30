84b604eb631a130721bcbdcee473181f161769e3
ruinername <russhianhere@gmail.com>
2019-04-30 23:01:49 +0300

    1.4.0-2

--- src/Components/Header/NavLink.js
+++ src/Components/Header/NavLink.js
@@ -1,7 +1,22 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types'
 
+/**
+ * NavLink component.
+ */
 
 export default class NavLink extends Component {
+  static propTypes = {
+    /** href */
+    href: PropTypes.string,
+    /** children */
+    children: PropTypes.any,
+    /** Default React className */
+    className: PropTypes.string,
+  }
+  static defaultProps = {
+    className: null,
+  }
 
   render() {
     return (
